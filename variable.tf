f5a11718f2c091c50c5c6a46c75ae0f73c508c7d
KNomamien <knomamien@gmail.com>
2023-07-28 02:54:52 +0100

    feat: Add RDS instance from saved DB snapshot
    Provisioned an RDS instance by restoring from a previously saved database snapshot.

--- variable.tf
+++ variable.tf
@@ -47,3 +47,28 @@ variable "ssh_location" {
     description = "this ip address can ssh into the EC2 instances"
     type        = string
 }
+
+# rds variables
+variable "database_snapshot_identifier" {
+    default     = "arn:aws:rds:us-east-1:284526353281:snapshot:dev-rds-db"
+    description = "the database snapshot arn"
+    type        = string
+}
+
+variable "database_instance_class" {
+    default     = "db.t2.micro"
+    description = "the database instance type"
+    type        = string
+}
+
+variable "database_instance_identifier" {
+    default     = "dev-rds-db"
+    description = "the database instance identifier"
+    type        = string
+}
+
+variable "multi_az_deployment" {
+    default     = false
+    description = "create a standby db instance"
+    type        = bool
+}
